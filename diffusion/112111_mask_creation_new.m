860eec5f875fdd427a85c3872341e0e9f985f259
plutoniusz <72443106+plutoniusz@users.noreply.github.com>
2023-07-16 22:27:22 +0100

    cleanup

--- diffusion/112111_mask_creation_new.m
+++
@@ -1,9 +0,0 @@
-% List of open inputs
-nrun = X; % enter the number of runs here
-jobfile = {'/data/underworld/home/kbas/project_nitorch_scribbles/112111_mask_creation_new_job.m'};
-jobs = repmat(jobfile, 1, nrun);
-inputs = cell(0, nrun);
-for crun = 1:nrun
-end
-spm('defaults', 'FMRI');
-spm_jobman('run', jobs, inputs{:});
--- diffusion/example_clustering.m
+++
@@ -1,6 +0,0 @@
-rng(0,'twister')  % For reproducibility
-mu1 = [1 2];
-sigma1 = [3 .2; .2 2];
-mu2 = [-1 -2];
-sigma2 = [2 0; 0 1];
-X = [mvnrnd(mu1,sigma1,200); mvnrnd(mu2,sigma2,100)];
